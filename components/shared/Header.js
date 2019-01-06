3831e10d0624ef594ef895df4c88a1f0a604a9a7
salvadomingo <salva@bluenoise.es>
2019-01-06 09:58:10 +0100

    Initial commit

---
+++ components/shared/Header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Link from 'next/link';
+
+import '../../styles/main.scss';
+
+class Header extends React.Component {
+
+  render() {
+
+    return (
+      <React.Fragment>
+
+        <p className='customClass'> I am styled P element </p>
+        <p className='customClassFromFile'> I am styled P element </p>
+
+        <Link href="/">
+          <a> Home </a>
+        </Link>
+
+        <Link href="/about">
+          <a> About </a>
+        </Link>
+
+        <Link href="/portfolios">
+          <a> Portfolio </a>
+        </Link>
+
+        <Link href="/blogs">
+          <a> Blog </a>
+        </Link>
+
+        <Link href="/cv">
+          <a> CV </a>
+        </Link>
+
+        <style jsx>
+          {`
+            a {
+              font-size: 20px;
+            };
+            .customClass {
+              color: red;
+            }
+            `
+          }
+        </style>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Header;
---
+++ pages/blogs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BaseLayout from '../components/layouts/BaseLayout';
+
+
+class Blogs extends React.Component {
+
+  render() {
+    return  (
+      <BaseLayout>
+        <h1> I am Blogs Page </h1>
+      </BaseLayout>
+    )
+  }
+
+}
+
+export default Blogs;
